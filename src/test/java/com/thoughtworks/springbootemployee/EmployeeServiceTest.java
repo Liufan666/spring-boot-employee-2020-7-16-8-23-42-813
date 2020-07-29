30ca90a9cfdf5ab664956c53e107ebc5afcdd702
Liufan666 <1663531467@qq.com>
2020-07-29 18:45:41 +0800

    [employeeJPA][vicky kevin] refactor: rename Class EmployeeServiceTest.

--- src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
@@ -16,7 +16,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
-public class EmployeeTest {
+public class EmployeeServiceTest {
 
     @Mock
     private EmployeeRepository employeeRepository;
