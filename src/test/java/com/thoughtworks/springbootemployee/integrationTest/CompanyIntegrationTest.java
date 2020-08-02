920eb0c1e4d98968169916ef63b5c93f977c1521
Liufan666 <1663531467@qq.com>
2020-08-02 15:06:02 +0800

    [employeeMockMvc][Domingo && Kevin] refactor: modify should_return_0_company_when_delete_1_company_given_1_company

--- src/test/java/com/thoughtworks/springbootemployee/integrationTest/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationTest/CompanyIntegrationTest.java
@@ -3,6 +3,7 @@ package com.thoughtworks.springbootemployee.integrationTest;
 import com.thoughtworks.springbootemployee.entity.Company;
 import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -28,6 +29,11 @@ public class CompanyIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @BeforeEach
+    void tearDown(){
+        companyRepository.deleteAll();
+    }
+
     @Test
     void should_return_ok_when_get_all_companies_given_0() throws Exception {
         //given
@@ -42,7 +48,6 @@ public class CompanyIntegrationTest {
     void should_add_one_company_when_add_new_company_given_valid_company() throws Exception {
         String companyJsonPayload = "{\n" +
                 "      \"name\": \"ooct\",\n" +
-                "      \"company_id\":2,\n" +
                 "      \"employees\": [{\n" +
                 "            \"id\": 1,\n" +
                 "            \"age\": 52,\n" +
@@ -54,16 +59,28 @@ public class CompanyIntegrationTest {
         mockMvc.perform(post("/companies").contentType(MediaType.APPLICATION_JSON).content(companyJsonPayload)).andExpect(status().is2xxSuccessful());
 
         List<Company> companies = companyRepository.findAll();
-        assertEquals("ooct",companies.stream().filter(e -> e.getCompanyId() == 2).findFirst().get().getName());
+        assertEquals(1,companies.size());
     }
 
     @Test
-    void should_return_1_company_when_delete_1_company_given_2_companies() throws Exception {
+    void should_return_0_company_when_delete_1_company_given_1_company() throws Exception {
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/companies/1"))
+        String companyJsonPayload = "{\n" +
+                "      \"name\": \"ooct\",\n" +
+                "      \"employees\": [{\n" +
+                "            \"id\": 1,\n" +
+                "            \"age\": 52,\n" +
+                "            \"name\": \"chengcheng\",\n" +
+                "            \"gender\": \"male\"\n" +
+                "        }]\n" +
+                "}";
+
+        mockMvc.perform(post("/companies").contentType(MediaType.APPLICATION_JSON).content(companyJsonPayload)).andExpect(status().is2xxSuccessful());
+        Integer companyId = companyRepository.findAll().get(0).getCompanyId();
+        mockMvc.perform(MockMvcRequestBuilders.delete("/companies/"+companyId))
                 .andExpect(status().is2xxSuccessful());
         List<Company> companyList = companyRepository.findAll();
-        assertEquals(1,companyList.size());
+        assertEquals(0,companyList.size());
     }
 
     @Test
